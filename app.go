7fa20b08decbf2ebe3731190bb098542a7fd838a
gebhartn <nicholas.gebhart@gmail.com>
2020-07-29 13:56:15 -0700

    feat: adds initialization method

--- app.go
+++ app.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"log"
 )
 
 // App main API
@@ -13,7 +15,19 @@ type App struct {
 }
 
 // Init method for the core application
-func (a *App) Init(user, password, dbname string) {}
+func (a *App) Init(user, password, dbname string) {
+	connectionString :=
+		fmt.Sprintf("postgres://%s:%s@localhost:5432/%s?sslmode=disable",
+			username, password, dbname)
+
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+}
 
 // Run application on given address
 func (a *App) Run(addr string) {}
